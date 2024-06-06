4ed7f32f40eb5780b91c9735d6138e518fd64387
engageintellect <jc9361@gmail.com>
2024-06-06 01:04:42 -0700

    updating readme

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,52 @@
-import Image from "next/image";
+import WordPullUp from "@/components/magicui/word-pull-up";
+import Technologies from "@/components/technologies";
+import BentoDemo from "@/components/bento";
+import ShimmerButton from "@/components/magicui/shimmer-button";
+import { Button } from "@/components/ui/button";
+import { VelocityScroll } from "@/components/magicui/scroll-based-velocity";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <>
+      <div className="w-full flex items-center justify-center max-w-3xl mx-auto">
+        <div className="flex flex-col items-center max-w-3xl mx-auto">
+          <WordPullUp words="Let's Cook" />
+          <p className="w-full max-w-lg py-5 px-6">
+            Ipsum exercitation mollit veniam ipsum laboris cupidatat irure quis
+            enim mollit esse fugiat incididunt pariatur.
+          </p>
+
+          <div className="flex items-center gap-2 w-full max-w-md mx-auto">
+            <a href="https://bento.engage-dev.com/projects" className="w-full">
+              <ShimmerButton className="shadow-xl w-full" borderRadius="10px">
+                <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+                  Browse Projects
+                </span>
+              </ShimmerButton>
+            </a>
+
+            <a href="mailto:engage_intellect@protonmail.com" className="w-full">
+              <ShimmerButton className="shadow-xl w-full" borderRadius="10px">
+                <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+                  Contact
+                </span>
+              </ShimmerButton>
+            </a>
+          </div>
+          <Technologies />
+
+          {/* <div className="my-20 max-w-3xl w-full mx-auto">
+            <VelocityScroll
+              text="Developer, Crypto, Stocks"
+              default_velocity={5}
+              className="font-display text-center text-4xl font-bold tracking-[-0.02em] text-black drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem]"
             />
-          </a>
+          </div> */}
+          <div className="w-full max-w-3xl mx-auto my-10">
+            <BentoDemo />
+          </div>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
---
+++ components/technologies.tsx
@@ -0,0 +1,52 @@
+import IconCloud from "@/components/magicui/icon-cloud";
+
+const slugs = [
+  "svelte",
+  "react",
+  "nextdotjs",
+  "nodedotjs",
+  "prisma",
+  "pocketbase",
+  "amazonaws",
+  "tailwindcss",
+  "graphql",
+  "apollo",
+  "mongodb",
+  "postgresql",
+  "mysql",
+  "redis",
+  "firebase",
+  "vercel",
+  "python",
+  "typescript",
+  "javascript",
+  "html5",
+  "css3",
+  "astro",
+  "linux",
+  "nginx",
+  "git",
+  "jira",
+  "github",
+  "gitlab",
+  "stripe",
+  "ethereum",
+  "solana",
+  "bitcoin",
+  "docker",
+  "ubuntu",
+  "openai",
+  "archlinux",
+  "pytorch",
+  "tensorflow",
+  "linode",
+  "kalilinux",
+];
+
+export default function Technologies() {
+  return (
+    <div className="max-w-1/2 w-full">
+      <IconCloud iconSlugs={slugs} />
+    </div>
+  );
+}
