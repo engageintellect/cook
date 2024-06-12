e4a3e483ae3cec01139b457943310834c2c09cef
engageintellect <jc9361@gmail.com>
2024-06-11 22:49:22 -0700

    fixing console errors

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { Bento } from "@/components/bento";
 export default function Home() {
   return (
     <>
-      {/* <Hero /> */}
       <div className="w-full flex items-center justify-center max-w-5xl mx-auto">
         <div className="flex flex-col items-center overflow-hidden">
           <div className="w-full py-2  px-2 lg:py-10 lg:px-4">
