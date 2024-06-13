c986c2d03e3cb59b89c53a64143772758cac61b9
engageintellect <jc9361@gmail.com>
2024-06-12 21:07:07 -0700

    updating readme

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
+export const dynamic = "force-static";
 
 export const metadata: Metadata = {
   title: "engage-cook",
