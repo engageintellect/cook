6d695cad706bfbc84d7d8568b803fb8402f76e42
engageintellect <jc9361@gmail.com>
2024-06-12 17:44:14 -0700

    updating readme

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,12 +18,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
-      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
