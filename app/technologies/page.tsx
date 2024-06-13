71c49266923c37ba0462d7a62d0623d7dffb86f1
engageintellect <jc9361@gmail.com>
2024-06-12 19:56:55 -0700

    working on technologies page

--- app/technologies/page.tsx
+++ app/technologies/page.tsx
@@ -2,7 +2,7 @@ import Technologies from "@/components/technologies";
 
 export default function Tech() {
   return (
-    <div className=" flex items-center w-full max-w-3xl mx-auto p-6 h-screen">
+    <div className=" flex items-center justify-center w-full max-w-3xl mx-auto p-6 h-screen">
       <div>
         <div className="flex flex-col items-start">
           <div className="text-4xl font-bold text-center">Technologies</div>
